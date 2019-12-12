9ca603d9c93e8726cf5362d9bdf3b1590e620fa3
Gary Engler <gengler@Garys-MBP.lan1>
2019-12-12 15:01:11 -0500

    Added Basic Logging Functinaliity

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import logging
 
-logger = logging.getLogger("__PYTHON_REST_API_TEMPLATE__")
+logger = logging.getLogger("SERVICE")
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s|%(name)-12s|%(levelname)-8s|%(message)s',
+    datefmt='%m-%d %H:%M',
+    filename='/temp/myapp.log',
+    filemode='w'
+)
 
 flask_app = Flask("__PYTHON_REST_API_TEMPLATE__")
 
